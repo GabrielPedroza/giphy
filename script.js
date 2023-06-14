294529904bc2798e63512a191e07c299b993c717
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-14 14:10:31 -0700

    fetched data from giphy api | displayed it to the DOM

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const formButton = document.querySelector('#btn')
+const formButton = document.querySelector('#btn');
 
-const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(e);
-}
+const API_KEY = 'cMV8oF7PtSGxAWgJaEyUUpAnN7fPNUPA';
 
-formButton.addEventListener("click", handleSubmit)
+formButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const searchInput = document.querySelector("#input").value;
+  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(searchInput)}&api_key=${API_KEY}&limit=5`;
+  
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Request failed');
+    
+    const data = await response.json();
+    displayData(data);
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
+});
+
+const displayData = (data) => {
+  const results = data.data;
+  const container = document.querySelector('#results-container');
+  
+  container.innerHTML = '';
+  
+  results.forEach((result) => {
+    const imageUrl = result.images.original.url;
+    const imageElement = document.createElement('img');
+    imageElement.src = imageUrl;
+    container.appendChild(imageElement);
+  });
+};
